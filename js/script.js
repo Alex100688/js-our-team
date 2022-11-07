6155d39d8b519cef02f42016244d863b3e6a041c
Alex100688 <alessiozingaro2@gmail.com>
2022-11-07 20:06:22 +0100

    Aggiunge un array e un ciclo for.

---
+++ js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+const ourTeam=[
+   {
+    fullName: 'Wayne Barnett',
+    job:'Founder & CEO',
+    immage: 'wayne-barnett-founder-ceo.jpg'
+   },
+   
+   {
+    fullName: 'Angela Caroll',
+    job:'Chief Editor',
+    immage: 'angela-caroll-chief-editor.jpg'
+   },
+   
+   {
+    fullName: 'Walter Gordon',
+    job:'Office Manager',
+    immage: 'walter-gordon-office-manager.jpg'
+   },
+
+   {
+    fullName: 'Angela Lopez',
+    job:'Social Media Manager',
+    immage: 'angela-lopez-social-media-manager.jpg'
+   },
+
+   {
+    fullName: 'Scott Estrada',
+    job:'Developer',
+    immage: 'scott-estrada-developer.jpg'
+   },
+
+   {
+    fullName: 'Barbara Ramos',
+    job:'Graphic Designer',
+    immage: 'barbara-ramos-graphic-designer.jpg'
+   }
+];
+console.log(ourTeam);
+for (let i = 0; i < ourTeam.length; i++) {
+    const team = ourTeam[i];
+    
+    console.log(team.fullName, team.job, team.immage);
+}
